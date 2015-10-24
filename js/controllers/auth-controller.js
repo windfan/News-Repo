c291b938100ee3da969015072a8e47d9a6f70392
Victor <ryu1031@gmail.com>
2015-10-23 22:25:22 -0400

    fix the login button issue
    fix the login button issue by using jQuery.
    adjust the onAuth method.

--- js/controllers/auth-controller.js
+++ js/controllers/auth-controller.js
@@ -2,19 +2,31 @@
 	'use strict';
 	angular.module('newsRepo')
 	
-	.controller("AuthController", ["$scope", '$firebaseArray', "$location",
-		function($scope, $location) {
+	.controller("AuthController", ["$scope", '$firebaseArray', "$window",
+		function($scope, $window) {
 			var ref = new Firebase("https://news-repo.firebaseio.com");
-			// var isNewUser = true;
-			// var vm = this;
-			// var vm = this;
-			// var isLoggedIn = false;
-			ref.onAuth(function(authData){
-				$scope.authData = authData;
-			});
+			$scope.authData = null;
+
+			var authDataCallback = function(authData) {
+				console.log("789");
+				if(authData === null) {
+					console.log("123");
+					$('#loginBtn').css('display', 'inline');
+					$('#logoutBtn').css('display', 'none');
+				}
+				else {
+					console.log("456");
+					$('#loginBtn').css('display', 'none');
+					$('#logoutBtn').css('display', 'inline');
+				}
+			}
+			//monitoring authentication status
+			ref.onAuth(authDataCallback);
+
 			$scope.login = function() {
+				// $scope.authData = null;
 				ref.authWithOAuthPopup("google", function(error, authData){
-					$scope.authData = authData;
+					// $scope.authData = authData;
 					if(error) {
 						// isLoggedIn = false;
 						if(error.code === "TRANSPORT_UNAVAILABLE") {
@@ -23,23 +35,38 @@
       						ref.authWithOAuthRedirect("google", function(error) { /* ... */ });
 						}
 					}
-					else if(authData) {
-						if(ref.child("profiles").child(authData.google.displayName) === null) {
-							ref.child("profiles").child(authData.uid).set({
-								"categories" : {1:"undefined"},
-								"hashtags" : {1: "News-Repo"},
-								"mail": getEmail(authData),
-								"name": getName(authData),
-								"news": {}
+					else if(authData !== null) {
+						var userExistsCallback = function(userID, exist) {
+							if(!exist) {
+								ref.child("profiles").child(authData.uid).set({
+									"categories" : {1:"undefined"},
+									"hashtags" : {1: "News-Repo"},
+									"mail": getEmail(authData),
+									"name": getName(authData),
+									"news": {}									
+								});
+							}
+						}
+
+						var checkIfUserExists = function(userID) {
+							ref.child("profiles").child(userID).once('value', function(snapshot){
+								var exist = (snapshot.val() !== null);
+								userExistsCallback(userID, exist);
 							});
 						}
+
+						
+						$scope.authData = authData;
+						checkIfUserExists(authData.uid);
+						// $window.location.reload(true);
+						//Test to see if /profiles/<userid> has any data
 						// isLoggedIn = true;
 						// $location.path = function(path) {
 						// 	return $location.path.apply($location, [path]);
 						// }
-						event.preventDefault();
-						$scope.$location.path('/home');
-						if(!$scope.$$phase) $scope.$apply()
+						// event.preventDefault();
+						// $scope.$location.path('/home');
+						// if(!$scope.$$phase) $scope.$apply()
 					}
 				}, {
 					remember: "sessionOnly",
@@ -57,8 +84,10 @@
 
 			$scope.logout = function() {
 				ref.unauth();
+				$scope.authData = null;
 				// isLoggedIn = false;
 			}
+
 		}
 	]);
 })();
